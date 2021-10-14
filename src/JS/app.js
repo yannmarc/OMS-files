d3d087836b81a93af722230c9f562e895a63f134
yannmarc <chamackstone@gmail.com>
2021-10-14 18:16:59 +0100

    commiting updates

--- src/JS/app.js
+++ src/JS/app.js
@@ -1,8 +1,10 @@
 const addBtn = document.getElementById('global-add');
 const addDropdown = document.querySelector('.add-dropdown');
 
-const newBtn = document.querySelector('.new');
+const newBtn = document.getElementById('new-customer');
 const modalContainer = document.querySelector('.modal-section');
+
+const closeBtn = document.querySelector('.bi-x-circle');
 
 addBtn.addEventListener('click', () =>{
     addDropdown.classList.toggle('show');
@@ -11,4 +13,8 @@ addBtn.addEventListener('click', () =>{
 newBtn.addEventListener('click', () => {
     modalContainer.classList.add('show');
     console.log("Hello Javascr")
+});
+
+closeBtn.addEventListener('click', () => {
+    modalContainer.classList.remove('show');
 })
